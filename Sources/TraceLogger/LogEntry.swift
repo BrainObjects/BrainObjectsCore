16fe5a21c90f21d586d46c847c4babfa84e4785c
Georg Tuparev <gtupar@tuparev.com>
2020-03-28 13:45:16 +0200

    Super early and simple initial implementation of LogEntry

--- Sources/TraceLogger/LogEntry.swift
+++ Sources/TraceLogger/LogEntry.swift
@@ -12,6 +12,35 @@
 
 import Foundation
 
-struct LogEntry {
-    
+/// LogEntry is the a data wrapper around a log message. It is lightweight and does not know how to propagate or how to
+/// display itself. These are tasks for the logger and for the channel.
+public struct LogEntry {
+
+    let level: Level
+
+    /// We use enhanced `os_log` message formats. See complete documentation for complete list
+    var message: String?
+
+    /// Many production systems relay on predefines table of error codes. Casual logging does not need codes.
+    var code: Int?
+
+
+    /// The levels are similar to Apple's `os_log` levels. Fatals feels like being a better name and we use it instead
+    /// of `fault`. `Warning` is additional level. The step between `error` and `default` is too wide. There are problems
+    /// that we can fix, but they are worth reporting and taking a special care. This is exactly the case for `warning`.
+    /// Channeled to `os_log` interface `warnings` perhaps should be converted to `default` logs.
+    public enum Level {
+        case fatal
+        case error
+        case warning
+        case `default`
+        case info
+        case debug
+    }
+
+    init(level: Level = .default, message: String?, code: Int?) {
+        self.level = level
+        self.message = message
+        self.code = code
+    }
 }
